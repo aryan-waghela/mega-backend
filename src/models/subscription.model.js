8ae2dc9c59dc25347dd52e7ace8c151d0595d747
aryan <aryanwaghela2905@gmail.com>
2026-02-10 00:34:23 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId, // one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId, // channel owner
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
