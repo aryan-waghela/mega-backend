b3e8b51ae7d5fc2a5bc72351a53113373704fa36
aryan <aryanwaghela2905@gmail.com>
2026-02-11 22:28:24 +0530

    Increase password security

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,7 +64,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 12);
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
