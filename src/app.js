ec78fa626c4e9e50e503cb810951d4bac522f0f5
aryan <aryanwaghela2905@gmail.com>
2026-02-14 16:48:16 +0530

    Add healthcheck functionality with routes and controller; implement server health status check

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -35,5 +36,6 @@ app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
